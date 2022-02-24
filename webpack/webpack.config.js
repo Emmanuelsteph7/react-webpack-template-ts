072b568dcb6746e77a9b5ec2db7a68c428c723c6
Emmanuelsteph7 <stephenemmanuel770@gmail.com>
2022-02-24 06:52:17 +0100

    created the initial react app template

---
+++ webpack/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  // resolve makes it possible to import a component without the extension.
+  // import Home from "./Home"
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"], // checks for tsx first, then ts, then js
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/, // searches for .ts, .tsx, .js, .jsx files
+        exclude: /node_modules/, // exclude node_modules
+        use: [{ loader: "babel-loader" }], // webpack should use babel-loader for all the specified files
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  mode: "development", // this sets process.env.NODE_ENV = development
+  plugins: [
+    //   this injects the bundle.js file into the html file and place it in the build folder
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+};
