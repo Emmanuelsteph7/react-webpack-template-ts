67cbb5747bee41449450eeab3165844757385899
Emmanuelsteph7 <stephenemmanuel770@gmail.com>
2022-02-25 06:08:57 +0100

    feat: added dev and production environments

---
+++ webpack/webpack.common.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  // resolve makes it possible to import a component without the extension.
+  // import Home from "./Home"
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"], // checks for tsx first, then ts, then js
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/, // searches for .ts, .tsx, .js, .jsx files
+        exclude: /node_modules/, // exclude node_modules
+        use: [{ loader: "babel-loader" }], // webpack should use babel-loader for all the specified files
+      },
+      {
+        test: /\.css$/, // searches for .css files
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i, // searches for the listed files
+        type: "asset/resource", // uses the asset resource module to handle them instead of imorting file-loader package
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/, // searches for fonts and svgs
+        type: "asset/inline", // use asset inline module instead of url-loader
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  // mode: "development", // this sets process.env.NODE_ENV = development
+  plugins: [
+    //   this injects the bundle.js file into the html file and place it in the build folder
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+};
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,43 +1,12 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const commonConfig = require("./webpack.common");
 
-module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
-  // resolve makes it possible to import a component without the extension.
-  // import Home from "./Home"
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"], // checks for tsx first, then ts, then js
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|js)x?$/, // searches for .ts, .tsx, .js, .jsx files
-        exclude: /node_modules/, // exclude node_modules
-        use: [{ loader: "babel-loader" }], // webpack should use babel-loader for all the specified files
-      },
-      {
-        test: /\.css$/, // searches for .css files
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i, // searches for the listed files
-        type: "asset/resource", // uses the asset module to handle them instead of imorting file-loader package
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-        type: "asset/inline",
-      },
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "bundle.js",
-  },
-  mode: "development", // this sets process.env.NODE_ENV = development
-  plugins: [
-    //   this injects the bundle.js file into the html file and place it in the build folder
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
-    }),
-  ],
+module.exports = (envVariables) => {
+  // using module.exports like this, we have access to the environment variables
+  // the env is set in the package.json file to determine dev or prod environments
+  const { env } = envVariables;
+  const envConfig = require(`./webpack.${env}.js`); // after getting the env, we get the necessary webpack config
+  const config = merge(commonConfig, envConfig);
+
+  return config;
 };
