9c9f7633e502a5bcbb9b8ac8812ea503f0a998ed
Emmanuelsteph7 <stephenemmanuel770@gmail.com>
2022-02-28 09:37:59 +0100

    feat: added extra plugins

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,43 +1,47 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  entry: path.resolve(__dirname, '..', './src/index.tsx'),
   // resolve makes it possible to import a component without the extension.
   // import Home from "./Home"
   resolve: {
-    extensions: [".tsx", ".ts", ".js"], // checks for tsx first, then ts, then js
+    extensions: ['.tsx', '.ts', '.js'], // checks for tsx first, then ts, then js
   },
   module: {
     rules: [
       {
         test: /\.(ts|js)x?$/, // searches for .ts, .tsx, .js, .jsx files
         exclude: /node_modules/, // exclude node_modules
-        use: [{ loader: "babel-loader" }], // webpack should use babel-loader for all the specified files
+        use: [{ loader: 'babel-loader' }], // webpack should use babel-loader for all the specified files
       },
       {
         test: /\.css$/, // searches for .css files
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i, // searches for the listed files
-        type: "asset/resource", // uses the asset resource module to handle them instead of imorting file-loader package
+        type: 'asset/resource', // uses the asset resource module to handle them instead of imorting file-loader package
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg)$/, // searches for fonts and svgs
-        type: "asset/inline", // use asset inline module instead of url-loader
+        type: 'asset/inline', // use asset inline module instead of url-loader
       },
     ],
   },
   output: {
-    path: path.resolve(__dirname, "..", "./build"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, '..', './build'),
+    filename: 'bundle.js',
   },
   // mode: "development", // this sets process.env.NODE_ENV = development
   plugins: [
     //   this injects the bundle.js file into the html file and place it in the build folder
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "..", "./src/index.html"),
+      template: path.resolve(__dirname, '..', './src/index.html'),
     }),
+    // new CopyPlugin({
+    //   patterns: [{ from: 'source', to: 'dest' }],
+    // }),
   ],
-};
+}
